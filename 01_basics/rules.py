217f22d01cde98809e4391c6e66cfdf9684618ac
ravi2krishna <ravi2krishna@gmail.com>
2025-08-29 08:03:25 +0530

    Rules - Identifiers

---
+++ 01_basics/rules.py
@@ -0,0 +1,32 @@
+# Python is a programming language
+# English -> We have alphabets --> words --> sentences 
+
+# Syntax / Rules / Guidelines
+
+# Means Comment -> Python will ignore this line
+
+a = 10
+print(a)
+
+# Collection of characters is called as String in python
+# Rule - strings should be enclosed in 'string' or "string"
+# greet = Good Morning
+# print(greet) # SyntaxError: invalid syntax
+greet = "Good Morning"
+print(greet) 
+
+# keywords -> get all the keywords
+# import keyword
+# print(keyword.kwlist)
+
+myname = "Ravi"
+print(myname)
+
+# 2myname = "Krishna" # SyntaxError: invalid syntax (cannot start with digit)
+# print(2myname) # SyntaxError: invalid syntax
+
+_myname = "Krishna"
+print(_myname)
+
+# class = 9 # SyntaxError: invalid syntax (class is a keyword)
+# print(class) # SyntaxError: invalid syntax
