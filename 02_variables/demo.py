c9939e3dd636a43a0a19303cf899132297630faa
ravi2krishna <ravi2krishna@gmail.com>
2025-08-29 08:26:20 +0530

    variables

---
+++ 02_variables/demo.py
@@ -0,0 +1,32 @@
+# Variables
+
+student_age = 20
+student_gpa = 8.5
+student_name = "Ravi"
+student_passed = True
+student_attendance = 20
+
+student_one_marks = [80,90,100]
+student_two_marks = [80,90,100]
+
+print(student_age)
+print(student_gpa)
+print(student_name)
+print(student_passed)
+print(student_attendance)
+
+print(id(student_age))
+print(id(student_gpa))
+print(id(student_name))
+print(id(student_passed))
+print(id(student_attendance))
+
+print(id(student_one_marks))
+print(id(student_two_marks))
+
+print(type(student_age))
+print(type(student_gpa))
+print(type(student_name))
+print(type(student_passed))
+print(type(student_attendance))
+print(type(student_one_marks))
